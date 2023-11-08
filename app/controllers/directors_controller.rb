cbc1868a47a21902498542d08b402e80d2b1ad07
tinacho12 <146214538+tinacho12@users.noreply.github.com>
2023-11-08 23:19:53 +0000

    Added youngest and eldest directors

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -13,4 +13,26 @@ class DirectorsController < ApplicationController
     render({ :template => "director_templates/details"})
   end
 
+  def youngest
+
+    @youngest_director_dob = Director.maximum(:dob)
+
+    youngest_director = Director.where({ :dob => @youngest_director_dob })
+
+    @youngest_director = youngest_director.at(0)
+
+    render({ :template => "director_templates/youngest"})
+  end
+
+  def eldest
+
+    @eldest_director_dob = Director.minimum(:dob)
+
+    eldest_director = Director.where({ :dob => @eldest_director_dob })
+
+    @eldest_director = eldest_director.at(0)
+
+    render({ :template => "director_templates/eldest"})
+  end
+
 end
